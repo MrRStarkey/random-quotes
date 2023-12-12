42bd08474345018423aba84e936ae06ce6dec355
MrRStarkey <mrrstarkey@gmail.com>
2023-12-12 15:29:41 +0000

    Updates

---
+++ precache-manifest.8c780cedf4797652b6e887962f826a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228dbe58ceaaf834955794230dde42b7",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "ff28f218e2e295ed0521",
+    "url": "/random-quotes/static/css/main.24440afa.chunk.css"
+  },
+  {
+    "revision": "da81daeebe4bd9aa0855",
+    "url": "/random-quotes/static/js/2.7530e007.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/random-quotes/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "ff28f218e2e295ed0521",
+    "url": "/random-quotes/static/js/main.f3266b6d.chunk.js"
+  },
+  {
+    "revision": "4401677efd933dcda067",
+    "url": "/random-quotes/static/js/runtime~main.bf4ec88a.js"
+  }
+]);
